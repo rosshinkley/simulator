01c7d5e300977bdd496e09b42828e76b4f457171
Ross <rosshinkley@gmail.com>
2016-05-14 01:46:56 -0400

    api test with junk request

--- test/api.js
+++ test/api.js
@@ -2,12 +2,15 @@ require('mocha-generators')
   .install();
 
 var chai = require('chai');
+var should = chai.should();
 var debug = require('debug')('segment-sim:test:lib');
 var data = require('./data');
 var app = require('../src/app');
 var got = require('got');
+var lib = require('../src/server/lib');
 
 describe('api', function() {
+  var base='http://localhost:3456/api'
   before(function(done) {
     app.listen(3456, done);
   });
@@ -20,30 +23,32 @@ describe('api', function() {
     .forEach(function(libName) {
       describe(`api for ${libName}`, function() {
         ['identify', 'track', 'page', 'alias', 'group'].forEach(function(method) {
-          it(`should run ${method} successfully`, function * () {
-            var result = (yield got([base, libname, method].join('/'), makeRequest('POST', data[method]))).body;
+          it.skip(`should run ${method} successfully`, function * () {
+            var result = (yield got([base, libName, method].join('/'), makeRequest('POST', data[method])))
+              .body;
             result.should.be.ok;
             result.result.should.be.okay;
-            chai.should.not.exist(result.err);
+            should.not.exist(result.err);
           });
 
-          it(`${method} should error on bad json`, function*() {
-            var result = (yield got([base, libname, method].join('/'), makeRequest('POST', 'this is bad json')));
-            result.body.should.be.ok;
-            result.body.err.should.be.ok;
-            chai.should.not.exist(result.result);
-            result.status.should.equal(400);
+          it(`${method} should error on bad json`, function (done) {
+            got([base, libName, method].join('/'), makeRequest('POST', 'this is bad json'))
+            .catch(error => {
+              error.statusCode.should.equal(400);
+              done()
+            })
           });
+
+          it.skip(`${method} should not accept bad component data`);
         });
 
-        it.skip(`${method} should not accept bad component data`);
+      });
+
+      it.skip(`${libName} should not allow bad method names`, function * () {
+        var result = (yield got([base, libName, 'junk'].join('/'), makeRequest('POST', data.identify)));
+        result.status.should.equal(400);
       });
     });
-
-  it.skip(`${libName} should not allow bad method names`, function*(){
-    var result = (yield got([base, libname, 'junk'].join('/'), makeRequest('POST', data.identify)));
-    result.status.should.equal.400;
-  });
 });
 
 function makeRequest(verb, body) {
